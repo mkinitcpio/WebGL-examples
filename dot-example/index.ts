b0c15809cae5879d6f87895125b6808c278b5aa5
mkinitcpio <wot220697@gmail.com>
2018-04-20 19:49:17 +0300

    Update fragment source code

--- dot-example/index.ts
+++ dot-example/index.ts
@@ -8,8 +8,13 @@ const vertexSource = `
 `;
 
 const fragmentSource = `
+    precision mediump float;
     void main(void) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        vec3 color;
+        color.r = 1.0;
+        color.g = 0.0;
+        color.b = 0.0;
+        gl_FragColor = vec4(color, 1.0);
     }
 `;
 
